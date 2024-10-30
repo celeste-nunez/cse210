4589c0042bb62ce94b32a10eea19fe52219a91a0
celeste-nunez <nun22002@byui.edu>
2024-10-30 09:47:36 -0600

    classDay1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,3 +7,17 @@ class Program
         Console.WriteLine("Hello Prep1 World!");
     }
 }
+
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        Console.Writeline("Hello, World!");
+    }
+}
+
+// double quotes and single quotes are not interchangeable
+// double quotes must be used to denote a string
+// must use semicolon to end function statements
+// squiggly wiggleys for code blocking functions
+// python interpreted, c# compiled
