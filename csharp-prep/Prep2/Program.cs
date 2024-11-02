3ddaf0ec33de3ab975168db0efb5a4573a19862d
celeste-nunez <nun22002@byui.edu>
2024-11-02 14:58:02 -0600

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage: ");
+        string gradeInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeInput);
+        
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade: {letter}");
+        
+
+        if (gradePercentage >70) 
+        {
+            Console.WriteLine("Congratulations! You passed the course. ");
+        }
+        else if (gradePercentage <70)
+        {
+            Console.WriteLine("Better luck next time. You failed the course.");
+        }
     }
 }
