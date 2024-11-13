3d508e34d7eec5b6ec599d6d2488b910224e9ef0
celeste-nunez <nun22002@byui.edu>
2024-11-13 09:50:53 -0700

    journal menu

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        int menuSelection;
+        do {
+            Console.WriteLine("Please select one of the following choices");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            menuSelection = int.Parse(Console.ReadLine());
+        } while (menuSelection != 5);
+
     }
 }
