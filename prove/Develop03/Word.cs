4d3c62757a6edac69137293cf79510c86c218d25
celeste-nunez <nun22002@byui.edu>
2024-11-20 09:51:21 -0700

    getting a random word

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,5 +1,4 @@
 using System.Runtime.CompilerServices;
-
 class Word 
 {
     private static List<string> ScriptureList()
@@ -12,14 +11,12 @@ class Word
             "ye",
             "shall",
             "have",
-            "tribulation",
-            ":",
+            "tribulation:",
             "but",
             "be",
             "of",
             "good",
-            "cheer",
-            ";",
+            "cheer;",
             "I",
             "have",
             "overcome",
@@ -35,6 +32,23 @@ class Word
         {
             Console.Write(word + " ");
         } 
+    }
+
+    private static List<int> GetRandomWords()
+    {
+        List<string> ScriptureWords = ScriptureList();
+        List<int> RandomWordInts = new List<int>{};
+        int loopCount = 0;
+
+        while (loopCount < 3)
+        {
+            Random random = new Random();
+            int index = random.Next(ScriptureWords.Count);
+            RandomWordInts.Add(index);
+            loopCount ++;
+        }
+
+        return RandomWordInts;
     }
 
     
